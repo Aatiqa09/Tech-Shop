adbd4ce3d99ffd07e183cb20cacd9f7a6270f3b5
Aatiqa09 <aatiqa0786@gmail.com>
2026-01-18 00:39:52 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Carousel from "./components/Carousel";
+import Carousel1 from "./components/Carousel1";
+import Footer from "./components/Footer";
+import ProductDetails from "./components/ProductDetails";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import { CartProvider } from "./context/CartContext";
+import ContactModal from "./components/ContactModal";
+import ProductCard from "./components/ProductCard";
+import RatingStars from "./components/RatingStars";
+
+export default function App() {
+  return (
+    <CartProvider>
+      <div className="bg-black min-h-screen text-white flex flex-col">
+        <Header />
+
+        <div className="flex-grow">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Carousel />
+                  <Carousel1 />
+                  <Home />
+
+                </>
+              }
+            />
+
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </CartProvider>
+  );
+}
